91eb772f3840fa77d90701f3b17d45ae4bd9edd7
Misko Hevery <misko@hevery.com>
2014-03-30 15:14:35 -0700

    WIP: repro case

--- example/web/hello_world.dart
+++ example/web/hello_world.dart
@@ -1,15 +1,31 @@
 import 'package:angular/angular.dart';
 import 'package:angular/angular_dynamic.dart';
+import 'dart:html';
 
 @NgController(
     selector: '[hello-world-controller]',
     publishAs: 'ctrl')
 class HelloWorldController {
-  String name = "world";
+  alert() => window.alert("Works");
+}
+
+@NgComponent(
+    selector: 'foo',
+    publishAs: 'foo',
+    template: '''
+      <button ng-click="foo.bar1()">One</button>
+      <button ng-click="foo.bar2()">Two</button>
+      ''')
+class Foo {
+  @NgCallback('bar')
+  Function bar1;
+  void bar2() {
+    bar1();
+  }
 }
 
 main() {
   ngDynamicApp()
-      .addModule(new Module()..type(HelloWorldController))
+      .addModule(new Module()..type(HelloWorldController)..type(Foo))
       .run();
 }
