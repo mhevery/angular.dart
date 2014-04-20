202133f31f900bbbd0c41efc0fe7e348365ce7f0
Marko Vuksanovic <markovuksanovic@gmail.com>
2014-04-20 23:10:03 +1000

    Modify hello world to try out stuff

--- example/web/hello_world.dart
+++ example/web/hello_world.dart
@@ -5,7 +5,27 @@ import 'package:angular/application_factory.dart';
     selector: '[hello-world-controller]',
     publishAs: 'ctrl')
 class HelloWorld {
-  String name = "world";
+  String name = "Otto, anna, marko";
+  List<String> _namesList = [];
+
+  List<String> get names {
+    var items;
+    name.split(',').forEach((n) {
+      if(!_isInNameList(n)) {
+        print("${n} is not in ${_namesList}");
+        _namesList.add(n);
+      }
+    });
+    return _namesList;
+  }
+
+  _isInNameList(String s) {
+    return _namesList.any((element) => element == s);
+  }
+
+  bool isPalindrome(String user) {
+    return user.toLowerCase().trim() == user.split('').reversed.join().toLowerCase().trim();
+  }
 }
 
 main() {
