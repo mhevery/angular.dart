d2d27e75f275344fde9c4ded53c9d337d424be89
Victor Berchet <victor@suumit.com>
2014-07-21 12:32:59 +0200

    feature(ngProbe): Allow specifying a root Node when using a selector

--- lib/introspection.dart
+++ lib/introspection.dart
@@ -64,18 +64,24 @@ List<ElementProbe> _findAllProbesInTree(dom.Node node) {
  *
  * The node parameter could be:
  * * a [dom.Node],
- * * a CSS selector for this node.
+ * * a CSS selector to look for a matching node inside the [root] or the dom.document.
  *
  * **NOTE:** This global method is here to make it easier to debug Angular
  * application from the browser's REPL, unit or end-to-end tests. The
  * function is not intended to be called from Angular application.
  */
-ElementProbe ngProbe(nodeOrSelector) {
-  if (nodeOrSelector == null) throw "ngProbe called without node";
+ElementProbe ngProbe(nodeOrSelector, [dom.Node root]) {
+  if (nodeOrSelector == null) throw "ngProbe called without node/selector";
   var node = nodeOrSelector;
   if (nodeOrSelector is String) {
-    var nodes = ngQuery(dom.document, nodeOrSelector);
-    node = (nodes.isNotEmpty) ? nodes.first : null;
+    if (root == null) {
+      root = dom.document;
+    } else {
+      root = new dom.DivElement()..append(root);
+    }
+    var nodes = ngQuery(root, nodeOrSelector);
+    if (nodes.isEmpty) throw "The '$nodeOrSelector' selector does not match any node";
+    node = nodes.first;
   }
   var probe = _findProbeWalkingUp(node);
   if (probe != null) {
@@ -93,7 +99,8 @@ ElementProbe ngProbe(nodeOrSelector) {
  * application from the browser's REPL, unit or end-to-end tests. The function
  * is not intended to be called from Angular application.
  */
-DirectiveInjector ngInjector(nodeOrSelector) => ngProbe(nodeOrSelector).injector;
+DirectiveInjector ngInjector(nodeOrSelector, [dom.Node root]) =>
+    ngProbe(nodeOrSelector, root).injector;
 
 
 /**
@@ -103,11 +110,10 @@ DirectiveInjector ngInjector(nodeOrSelector) => ngProbe(nodeOrSelector).injector
  * application from the browser's REPL, unit or end-to-end tests. The function
  * is not intended to be called from Angular application.
  */
-Scope ngScope(nodeOrSelector) => ngProbe(nodeOrSelector).scope;
+Scope ngScope(nodeOrSelector, [dom.Node root]) => ngProbe(nodeOrSelector, root).scope;
 
 
-List<dom.Element> ngQuery(dom.Node element, String selector,
-                          [String containsText]) {
+List<dom.Element> ngQuery(dom.Node element, String selector, [String containsText]) {
   var list = [];
   var children = [element];
   if ((element is dom.Element) && element.shadowRoot != null) {
