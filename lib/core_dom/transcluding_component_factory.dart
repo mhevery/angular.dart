970611dcd1b6857cbc7c656fd4d7785e96de3a42
Misko Hevery <misko@hevery.com>
2014-08-14 19:52:27 -0700

    perf(view): increase view instantiation speed 40%
    1) Stop using futures when the value is already cached
    2) Remove adding nodes to fake parent during view instantiation
    
    Closes #1358

--- lib/core_dom/transcluding_component_factory.dart
+++ lib/core_dom/transcluding_component_factory.dart
@@ -20,17 +20,18 @@ class BoundTranscludingComponentFactory implements BoundComponentFactory {
   final Injector _injector;
 
   Component get _component => _ref.annotation as Component;
-  async.Future<ViewFactory> _viewFuture;
+  async.Future<ViewFactory> _viewFactoryFuture;
+  ViewFactory _viewFactory;
 
   BoundTranscludingComponentFactory(this._f, this._ref, this._directives, this._injector) {
-    _viewFuture = BoundComponentFactory._viewFuture(
-        _component,
-        _f.viewCache,
-        _directives);
+    _viewFactoryFuture = BoundComponentFactory._viewFactoryFuture(_component, _f.viewCache, _directives);
+    if (_viewFactoryFuture != null) {
+      _viewFactoryFuture.then((viewFactory) => _viewFactory = viewFactory);
+    }
   }
 
   List<Key> get callArgs => _CALL_ARGS;
-  static var _CALL_ARGS = [ DIRECTIVE_INJECTOR_KEY, SCOPE_KEY, VIEW_KEY,
+  static var _CALL_ARGS = [ DIRECTIVE_INJECTOR_KEY, SCOPE_KEY,
                             VIEW_CACHE_KEY, HTTP_KEY, TEMPLATE_CACHE_KEY,
                             DIRECTIVE_MAP_KEY, NG_BASE_CSS_KEY, EVENT_HANDLER_KEY];
   Function call(dom.Node node) {
@@ -39,55 +40,42 @@ class BoundTranscludingComponentFactory implements BoundComponentFactory {
            _component.cssUrls.isEmpty);
 
     var element = node as dom.Element;
-    return (DirectiveInjector injector, Scope scope, View view,
+    var component = _component;
+    return (DirectiveInjector injector, Scope scope,
             ViewCache viewCache, Http http, TemplateCache templateCache,
             DirectiveMap directives, NgBaseCss baseCss, EventHandler eventHandler) {
 
-      DirectiveInjector childInjector;
-      var childInjectorCompleter; // Used if the ViewFuture is available before the childInjector.
-
-      var component = _component;
+      List<async.Future> futures = [];
       var lightDom = new LightDom(element, scope);
-
-      // Append the component's template as children
-      var elementFuture;
-
-      if (_viewFuture != null) {
-        elementFuture = _viewFuture.then((ViewFactory viewFactory) {
-          lightDom.pullNodes();
-
-          if (childInjector != null) {
-            lightDom.shadowDomView = viewFactory.call(childInjector.scope, childInjector);
-            return element;
-          } else {
-            childInjectorCompleter = new async.Completer();
-            return childInjectorCompleter.future.then((childInjector) {
-              lightDom.shadowDomView = viewFactory.call(childInjector.scope, childInjector);
-              return element;
-            });
-          }
-        });
-      } else {
-        elementFuture = new async.Future.microtask(() => lightDom.pullNodes());
-      }
-      TemplateLoader templateLoader = new TemplateLoader(elementFuture);
-
+      TemplateLoader templateLoader = new TemplateLoader(element, futures);
       Scope shadowScope = scope.createChild(new HashMap());
-
-      childInjector = new ComponentDirectiveInjector(injector, this._injector,
-          eventHandler, shadowScope, templateLoader, new EmulatedShadowRoot(element), lightDom, view);
-
+      DirectiveInjector childInjector = new ComponentDirectiveInjector(
+          injector, this._injector, eventHandler, shadowScope, templateLoader,
+          new EmulatedShadowRoot(element), lightDom);
       childInjector.bindByKey(_ref.typeKey, _ref.factory, _ref.paramKeys, _ref.annotation.visibility);
-
-      if (childInjectorCompleter != null) {
-        childInjectorCompleter.complete(childInjector);
-      }
 
       var controller = childInjector.getByKey(_ref.typeKey);
       shadowScope.context[component.publishAs] = controller;
       if (controller is ScopeAware) controller.scope = shadowScope;
       BoundComponentFactory._setupOnShadowDomAttach(controller, templateLoader, shadowScope);
+
+      if (_viewFactoryFuture != null && _viewFactory == null) {
+        futures.add(_viewFactoryFuture.then((ViewFactory viewFactory) =>
+            _insert(viewFactory, element, childInjector, lightDom)));
+      } else {
+        scope.rootScope.runAsync(() {
+          _insert(_viewFactory, element, childInjector, lightDom);
+        });
+      }
       return controller;
     };
+  }
+
+  _insert(ViewFactory viewFactory, dom.Element element, DirectiveInjector childInjector,
+          LightDom lightDom) {
+    lightDom.pullNodes();
+    if (viewFactory != null) {
+      lightDom.shadowDomView = viewFactory.call(childInjector.scope, childInjector);
+    }
   }
 }
--- test/core_dom/compiler_spec.dart
+++ test/core_dom/compiler_spec.dart
@@ -558,22 +558,6 @@ void main() {
         expect(element).toHaveText('inside ');
       }));
 
-      it('should not pass null to a inner directives', async((Logger logger) {
-        _.compile('<div>'
-                    '<once-inside ng-repeat="x in nn" v="b"></once-inside>'
-                  '</div>');
-
-        _.rootScope.context['nn'] = [1];
-        _.rootScope.apply();
-        microLeap();
-
-        _.rootScope.context['nn'].add(2);
-        _.rootScope.apply();
-        microLeap();
-
-        expect(logger.contains(null)).toBeFalsy();
-      }));
-
       it('should create a component with I/O', async(() {
          _.compile(r'<div><io attr="A" expr="name" ondone="done=true"></io></div>');
         microLeap();
@@ -1030,7 +1014,7 @@ void main() {
       }));
 
       /*
-        This test is dissabled becouse I (misko) thinks it has no real use case. It is easier
+        This test is disabled because I (misko) thinks it has no real use case. It is easier
         to understand in terms of ng-repeat
 
           <tabs>
@@ -1044,7 +1028,7 @@ void main() {
           the DirectChild between tabs and pane.
 
           It is not clear to me (misko) that there is a use case for getting hold of the
-          tranrscluding directive such a ng-repeat.
+          transcluding directive such a ng-repeat.
        */
       xit('should reuse controllers for transclusions', async((Logger log) {
         _.compile('<div simple-transclude-in-attach include-transclude>view</div>');
